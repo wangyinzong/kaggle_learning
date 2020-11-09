87943c66ab19c1df53412f271c325593498c85e7
wangyinzong <1370818974@qq.com>
2020-11-09 18:09:36 +0800

    h

--- Getting_Started_with_Titanic.py
+++ Getting_Started_with_Titanic.py
@@ -12,6 +12,5 @@ import os
 for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
-
 # You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All"
 # You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
